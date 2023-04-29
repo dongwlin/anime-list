fa57121be89487b27ac30f64f6ae2d105e4bf1f7
Dongw <picklinguo@gmail.com>
2023-04-29 17:54:09 +0800

    feat: 引入pinia管理状态

--- anime-list/src/main.js
+++ anime-list/src/main.js
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 import router from "@/router/index.js";
 import ElementPlus from "element-plus";
 import "element-plus/theme-chalk/index.css";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
 app.use(router);
+app.use(createPinia());
 app.use(ElementPlus);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
---
+++ anime-list/src/store/index.js
@@ -0,0 +1,69 @@
+import { defineStore } from 'pinia';
+
+export const useAnimeOptions = defineStore('main', {
+    state: () => {
+        return {
+            status: [
+                {
+                    label: 'true',
+                    value: true
+                },
+                {
+                    label: 'false',
+                    value: false
+                }
+            ],
+            optionsType: [
+                {
+                    label: 'none',
+                    value: -1
+                },
+                {
+                    label: 'network',
+                    value: 0
+                },
+                {
+                    label: 'local',
+                    value: 1
+                }
+            ],
+            optionsDay: [
+                {
+                    label: 'none',
+                    value: -1
+                },
+                {
+                    label: 'Sunday',
+                    value: 0
+                },
+                {
+                    label: 'Monday',
+                    value: 1
+                },
+                {
+                    label: 'Tuesday',
+                    value: 2
+                },
+                {
+                    label: 'Wednesday',
+                    value: 3
+                },
+                {
+                    label: 'Thursday',
+                    value: 4
+                },
+                {
+                    label: 'Friday',
+                    value: 5
+                },
+                {
+                    label: 'Saturday',
+                    value: 6
+                }
+            ]
+        }
+    },
+    actions: {
+
+    }
+})
