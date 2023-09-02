cdd92ae7ad0f04ed3e39f7c33adf0cb4c0c6ede9
Dongw <linsann@163.com>
2023-09-03 06:17:02 +0800

    refactor: 重构apiResult

--- AnimeList/apiResult.hpp
+++ AnimeList/apiResult.hpp
@@ -3,31 +3,33 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
-std::string api_result(std::string msg = "fail", nlohmann::json data = nlohmann::json::array(), int code = 500)
+using json = nlohmann::json;
+
+std::string api_result(std::string msg = "fail", json data = json::array(), int code = 500)
 {
-	nlohmann::json result;
+	json result;
 	result["code"] = code;
 	result["msg"] = msg;
 	result["data"] = data;
 	return result.dump();
 }
 
-std::string api_success(nlohmann::json data = nlohmann::json::array(), std::string msg = "success", int code = 200)
+std::string api_success(json data = json::array(), std::string msg = "success", int code = 200)
 {
 	return api_result(msg, data, code);
 }
 
-std::string api_error(std::string msg = "fail", nlohmann::json data = nlohmann::json::array(),  int code = 500)
+std::string api_error(std::string msg = "fail", json data = json::array(),  int code = 500)
 {
 	return api_result(msg, data, code);
 }
 
-std::string api_error_clinet(std::string msg = "client error", nlohmann::json data = nlohmann::json::array(), int code = 400)
+std::string api_bad_request(std::string msg = "bad request", json data = json::array(), int code = 400)
 {
 	return api_result(msg, data, code);
 }
 
-std::string api_error_404(std::string msg = "not found", nlohmann::json data = nlohmann::json::array(), int code = 404)
+std::string api_error_404(std::string msg = "not found", json data = json::array(), int code = 404)
 {
 	return api_result(msg, data, code);
 }
