36fa348a3895bbf5b9f3ee011e03656eb54d5dfc
dongwlin <dongwlin@foxmail.com>
2024-07-15 23:42:13 +0800

    chore: fix call of reflect.Value.FieldByName on ptr Value

--- internal/pkg/util/sqlite3Err.go
+++ internal/pkg/util/sqlite3Err.go
@@ -7,7 +7,7 @@ import (
 )
 
 func SetErrCode(sqlite3Err *sqlite3.Error, code sqlite3.ErrorCode) {
-	codeField := reflect.ValueOf(&sqlite3Err).Elem().FieldByName("code")
+	codeField := reflect.ValueOf(sqlite3Err).Elem().FieldByName("code")
 	v := reflect.NewAt(codeField.Type(), unsafe.Pointer(codeField.UnsafeAddr())).Elem()
 	codeValue := reflect.ValueOf(uint64(code))
 	v.Set(codeValue)
